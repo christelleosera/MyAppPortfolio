23c7c0b8ac803704f07aec18f87c996f05bcf8e3
Christelle Osera <christelle.osera@adcolony.com>
2015-07-15 16:21:48 -0700

    followed submission notes

--- app/src/main/java/christelleosera/myappportfolio/MainActivity.java
+++ app/src/main/java/christelleosera/myappportfolio/MainActivity.java
@@ -16,81 +16,69 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button spotify_button = (Button) findViewById(R.id.spotify_button);
-        spotify_button.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                CharSequence text = "Spotify";
-                int duration = Toast.LENGTH_SHORT;
-
-                if(toast == null) toast = Toast.makeText(context, text, duration);
-                toast.setText(text);
-                toast.show();
-            }
-        });
-        Button scores_button = (Button) findViewById(R.id.scores_button);
-        scores_button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                CharSequence text = "Scores";
-                int duration = Toast.LENGTH_SHORT;
-
-                if(toast == null) toast = Toast.makeText(context, text, duration);
-                toast.setText(text);
-                toast.show();
-            }
-        });
-        Button library_button = (Button) findViewById(R.id.library_button);
-        library_button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                CharSequence text = "Library";
-                int duration = Toast.LENGTH_SHORT;
-
-                if(toast == null) toast = Toast.makeText(context, text, duration);
-                toast.setText(text);
-                toast.show();
-            }
-        });
-        Button build_button = (Button) findViewById(R.id.build_button);
-        build_button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                CharSequence text = "Build";
-                int duration = Toast.LENGTH_SHORT;
-
-                if(toast == null) toast = Toast.makeText(context, text, duration);
-                toast.setText(text);
-                toast.show();
-            }
-        });
-        Button xyz_button = (Button) findViewById(R.id.xyz_button);
-        xyz_button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                CharSequence text = "XYZ";
-                int duration = Toast.LENGTH_SHORT;
-
-                if(toast == null) toast = Toast.makeText(context, text, duration);
-                toast.setText(text);
-                toast.show();
-            }
-        });
-        Button capstone_button = (Button) findViewById(R.id.capstone_button);
-        capstone_button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                CharSequence text = "Capstone";
-                int duration = Toast.LENGTH_SHORT;
-
-                if(toast == null) toast = Toast.makeText(context, text, duration);
-                toast.setText(text);
-                toast.show();
-            }
-        });
     }
+
+    public void showToastSpotify(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "Spotify";
+        int duration = Toast.LENGTH_SHORT;
+
+        if (toast == null) toast = Toast.makeText(context, text, duration);
+        toast.setText(text);
+        toast.show();
+    }
+
+    public void showToastScores(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "Scores";
+        int duration = Toast.LENGTH_SHORT;
+
+        if (toast == null) toast = Toast.makeText(context, text, duration);
+        toast.setText(text);
+        toast.show();
+    }
+
+    public void showToastLibrary(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "Library";
+        int duration = Toast.LENGTH_SHORT;
+
+        if (toast == null) toast = Toast.makeText(context, text, duration);
+        toast.setText(text);
+        toast.show();
+    }
+
+    public void showToastBuild(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "Build";
+        int duration = Toast.LENGTH_SHORT;
+
+        if (toast == null) toast = Toast.makeText(context, text, duration);
+        toast.setText(text);
+        toast.show();
+    }
+
+    public void showToastXyz(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "XYZ";
+        int duration = Toast.LENGTH_SHORT;
+
+        if (toast == null) toast = Toast.makeText(context, text, duration);
+        toast.setText(text);
+        toast.show();
+    }
+
+    public void showToastCapstone(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "Capstone";
+        int duration = Toast.LENGTH_SHORT;
+
+        if (toast == null) toast = Toast.makeText(context, text, duration);
+        toast.setText(text);
+        toast.show();
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
